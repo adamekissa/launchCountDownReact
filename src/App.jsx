2742f072162ce2bd779e015bdbf49d1fe3b5727c
Mohammed Adam <adamekissa@gmail.com>
2024-06-17 16:15:15 +0100

    Formatting done

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,24 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(5);
-  const display = updateDisplay(count)
+  const display = generateCountOrEmoji(count)
 
-  function decrease(){
-    if (count >= 1){
+  function decrease() {
+    if (count >= 1) {
       setCount((count) => count - 1)
-    } else{
+    } else {
       setCount(0)
-  
-  }
-}
 
-function reset(){
-  setCount(5);
-}
-  function updateDisplay(b){
-    if (b === 0 ){
+    }
+  }
+
+  function reset() {
+    setCount(5);
+  }
+  function generateCountOrEmoji(b) {
+    if (b === 0) {
       return "🚀"
-    } else if (b === 1 ){
+    } else if (b === 1) {
       return "👩🏽‍🚀"
     } else {
       return b
@@ -29,9 +29,9 @@ function reset(){
 
   return (
     <>
-    <h3>Launch Count Down App - Mohammed</h3>
+      <h3>Launch Count Down App - Mohammed</h3>
       <div className="card">
-        <p className="diplay" style={{fontSize: 50}}>{display}</p>
+        <p className="diplay" style={{ fontSize: 50 }}>{display}</p>
         <p>{count}</p>
         <button onClick={decrease}>
           Decrease
